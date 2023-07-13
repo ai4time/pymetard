6692dc0d3d08757287f72ae6256db705973599e8
yipengh <huang.ypeng@gmail.com>
2023-07-13 17:41:35 +0800

    refact: no more needs for timers and old abstract downloader

--- ingestion/__init__.py
+++ ingestion/__init__.py
@@ -1,20 +1,3 @@
-from ingestion.downloader import (
-    AbstractDownloader,
-    MrmsDownloader,
-    MrmsIsuDownloader,
-    TjwfSimulatedDownloader,
-)
-
-from ingestion.timer import (
-    round_down,
-    Timer,
-    TimerBounds,
-
-    FixedDateRealTimeTimer,
-    MrmsTimer,
-    TjwfTimer,
-)
-
 from ingestion.awc import (
     fetch_stations,
     AviationWeatherCenterMetarDownloader,
